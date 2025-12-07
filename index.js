6b93259ff358b0b878b44b277a1e0a6938ab3ac5
Eliza Kolokoltsova <eliza.kolokoltsova@yandex.ru>
2025-12-07 20:11:36 +0500

    Сдача

--- index.js
+++ index.js
@@ -12,24 +12,62 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	if (localStorage.getItem('tasks')) {
+		return JSON.parse(localStorage.getItem('tasks'));
+	} else {
+		return items;
+	}
 }
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
-
+	const textElement = clone.querySelector(".to-do__item-text");
+	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+	deleteButton.addEventListener('click', function (evt) {
+		clone.remove();
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+	duplicateButton.addEventListener('click', function (evt) {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+	const editButton = clone.querySelector(".to-do__item-button_type_edit");
+	editButton.addEventListener('click', () => {
+		textElement.setAttribute('contenteditable', 'true');
+		textElement.focus();
+	});
+	textElement.addEventListener('blur', function () {
+		textElement.setAttribute('contenteditable', 'false');
+		saveTasks(getTasksFromDOM());
+	})
+	textElement.textContent = item;
+	return clone;
 }
 
 function getTasksFromDOM() {
-
+	const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
+	const tasks = [];
+	itemsNamesElements.forEach(item => tasks.push(item.textContent));
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+items = loadTasks()
+items.forEach(item => listElement.append(createItem(item)));
+
+formElement.addEventListener('submit', function (evt) {
+	evt.preventDefault();
+	listElement.prepend(createItem(inputElement.value));
+	items = getTasksFromDOM();
+	saveTasks(items);
+	formElement.reset();
+})
